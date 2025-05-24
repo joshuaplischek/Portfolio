bdad3d3c6f2e5e3bbed2201b76de805536545fb7
Joshua Plischek <plischekjoshua@web.de>
2025-05-24 17:15:23 +0200

    import header to hero section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
