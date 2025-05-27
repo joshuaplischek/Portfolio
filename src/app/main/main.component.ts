d6cd4780e44d1387540abcff6a07a9f02c00ad56
Joshua Plischek <plischekjoshua@web.de>
2025-05-27 20:53:13 +0200

    add skill section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsComponent } from "../skills/skills.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent,AboutMeComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
