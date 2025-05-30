ad78f006824980999b1f4b87f0996b922ad8dad3
Joshua Plischek <plischekjoshua@web.de>
2025-05-30 18:04:12 +0200

    finished testimonials + add contact component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,10 +4,11 @@ import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from "../skills/skills.component";
 import { MyProjectsComponent } from "../my-projects/my-projects.component";
 import { TestimonialsComponent } from "../testimonials/testimonials.component";
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent, MyProjectsComponent, TestimonialsComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, MyProjectsComponent, TestimonialsComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
