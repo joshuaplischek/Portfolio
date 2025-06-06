22b87245bc3d7336a73f07eff3a9f7578a3053bc
Joshua Plischek <plischekjoshua@web.de>
2025-06-06 09:48:09 +0200

    generate Project template

---
+++ src/app/my-projects/project/project-template/project-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-template',
+  imports: [],
+  templateUrl: './project-template.component.html',
+  styleUrl: './project-template.component.scss'
+})
+export class ProjectTemplateComponent {
+
+}
