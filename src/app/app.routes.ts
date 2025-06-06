0a0d47586977213b758105e73737b0143b4fa7a8
Joshua Plischek <plischekjoshua@web.de>
2025-06-06 12:43:31 +0200

    routerlink the projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { JoinComponent } from './my-projects/join/join.component';
+import { SharkieComponent } from './my-projects/sharkie/sharkie.component';
+import { PokeDexComponent } from './my-projects/poke-dex/poke-dex.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    { path: 'join', component: JoinComponent},
+    { path: 'sharkie', component: SharkieComponent},
+    { path: 'pokedex', component: PokeDexComponent},
 ];
--- src/app/my-projects/project/project.component.ts
+++ src/app/my-projects/project/project.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-project',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
 })
 export class ProjectComponent {
+  @Input() image: string = '';
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input() image: string = '';
+  @Input() projectLink: string = '';
 }
