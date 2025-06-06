fbd2baaef745d4f00d993dfbb70a3cb8d525cad0
Joshua Plischek <plischekjoshua@web.de>
2025-06-06 12:24:18 +0200

    design button in projects component

---
+++ src/app/my-projects/poke-dex/poke-dex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-dex',
+  imports: [],
+  templateUrl: './poke-dex.component.html',
+  styleUrl: './poke-dex.component.scss'
+})
+export class PokeDexComponent {
+
+}
---
+++ src/app/my-projects/sharkie/sharkie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sharkie',
+  imports: [],
+  templateUrl: './sharkie.component.html',
+  styleUrl: './sharkie.component.scss'
+})
+export class SharkieComponent {
+
+}
