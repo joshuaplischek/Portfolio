e211cdf124c1cf726a2ede58cc0b9bca76c67f25
Joshua Plischek <plischekjoshua@web.de>
2025-06-19 12:50:30 +0200

    fix link to legal notice

--- src/app/contact/footer/footer.component.ts
+++ src/app/contact/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterLink],
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
