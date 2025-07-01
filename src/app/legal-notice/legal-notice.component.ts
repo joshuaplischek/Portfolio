ea5f3c92b4a86809ffd86eced2d391acaa9ecf5a
joshuaplischek <plischekjoshua@web.de>
2025-07-01 18:47:48 +0200

    add burger Menu to legal Notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { TranslatePipe } from '@ngx-translate/core';
+import { BurgerMenuComponent } from '../burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [HeaderComponent, TranslatePipe],
+  imports: [HeaderComponent, TranslatePipe, BurgerMenuComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  menuOpen = false;
 
+  closeMenu(header: HeaderComponent) {
+    this.menuOpen = false;
+    header.resetBurgerMenu();
+  }
+
+  onToggleMenu(isOpen: boolean) {
+    this.menuOpen = isOpen;
+  }
 }
