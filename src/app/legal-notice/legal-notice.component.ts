433f103deabdf8f758de842380c880ee85b5ccd3
joshuaplischek <plischekjoshua@web.de>
2025-07-01 18:36:54 +0200

    add styling to ledal-notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [HeaderComponent, TranslatePipe],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
