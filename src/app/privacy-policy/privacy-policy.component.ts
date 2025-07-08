54d3fffd00a056870a788168c5d7460fcf2181e6
joshuaplischek <plischekjoshua@web.de>
2025-07-08 14:00:41 +0200

    Finishing header and Burger menu in privacy policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HeaderComponent } from '../header/header.component';
+import { BurgerMenuComponent } from "../burger-menu/burger-menu.component";
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [TranslatePipe, HeaderComponent],
+  imports: [TranslatePipe, HeaderComponent, BurgerMenuComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+    menuOpen = false;
 
+  closeMenu(header: HeaderComponent) {
+    this.menuOpen = false;
+    header.resetBurgerMenu();
+  }
+
+  onToggleMenu(isOpen: boolean) {
+    this.menuOpen = isOpen;
+  }
 }
