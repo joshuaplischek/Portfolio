bb0a48c472bb95f9f19694ea7a60fb3c8e5ad57e
joshuaplischek <plischekjoshua@web.de>
2025-07-19 00:16:53 +0200

    add user feeback for send mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -24,6 +24,7 @@ export class ContactComponent {
   http = inject(HttpClient);
   isSubmitting = false;
   mailTest = false;
+  showSuccessMessage = false;
 
 onMouseEnterBtn(element: HTMLElement, form: NgForm) {
   if (form.valid && this.contactData.privacy) {
@@ -59,20 +60,31 @@ onMouseLeaveBtn(element: HTMLElement, form: NgForm) {
 
   onSubmit(ngForm: NgForm, element: HTMLElement, form: NgForm) {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+      this.http.post(this.post.endPoint, this.post.body(this.contactData), this.post.options)
         .subscribe({
           next: (response) => {
-            this.onMouseLeaveBtn(element, form)
+            this.onMouseLeaveBtn(element, form);
             ngForm.resetForm();
+            this.showSuccessMessage = true; // Overlay anzeigen
+            
+            // Optional: Overlay nach 5 Sekunden automatisch schließen
+            setTimeout(() => {
+              this.showSuccessMessage = false;
+            }, 5000);
           },
           error: (error) => {
             console.error(error);
+            // Hier könntest du auch ein Error-Overlay anzeigen
           },
           complete: () => console.info('send mail complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
       ngForm.resetForm();
+      this.showSuccessMessage = true; // Auch für Test-Modus
     }
+  }
+
+  closeSuccessMessage() {
+    this.showSuccessMessage = false;
   }
 }
